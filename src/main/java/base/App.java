ac091197d46051ceac1e1858cfe731ce2515743e
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 19:33:54 -0400

    Solution to exercise 11

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,9 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.util.Scanner;
+
 /*
 Write a program that converts currency. Specifically, convert euros to U.S. dollars. Prompt for the amount of money in
 euros you have, and prompt for the current exchange rate of the euro. Print out the new amount in U.S. dollars.
@@ -35,7 +38,31 @@ Build a dictionary of conversion rates and prompt for the countries instead of t
 Wire up your application to an external API that provides the current exchange rates.
  */
 public class App {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        int euros = readEuroAmount();
+        float rate = currentExchangeRate();
+        float dollarsUS = getUSDollarAmount(euros, rate);
+        generateOutput(euros, rate, dollarsUS);
+    }
+
+    private static void generateOutput(int euros, float rate, float dollarsUS) {
+        System.out.println(String.format("How many euros are you exchanging? %d" +
+                                        "\nWhat is the exchange rate? %.4f\n%d euros " +
+                                        "at an exchange rate of %.4f is" +
+                                        "\n%.2f U.S. dollars.", euros, rate, euros, rate, dollarsUS));
+    }
+
+    private static float getUSDollarAmount(int euros, float rate) {
+        return rate * euros;
+    }
+
+    private static float currentExchangeRate() {
+        return in.nextFloat();
+    }
+
+    private static int readEuroAmount() {
+        return in.nextInt();
     }
 }
